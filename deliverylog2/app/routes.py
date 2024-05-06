01fe14612d4c59e6105132230c993eecbf686b66
CaptainWalrus16 <131814203+CaptainWalrus16@users.noreply.github.com>
2024-05-06 12:31:19 -0400

    Add files via upload

---
+++ deliverylog2/app/routes.py
@@ -0,0 +1,60 @@
+from flask import render_template, flash, redirect, url_for, request
+from app import app
+from app.foms import LoginForm, RegistrationForm
+from flask_login import current_user, login_user, logout_user, login_required
+import sqlalchemy as sa
+from app import db
+from app.models import User
+from urllib.parse import urlsplit
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = db.session.scalar(
+            sa.select(User).where(User.username == form.username.data)
+        )
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or urlsplit(next_page).netloc != '': next_page = url_for('index')
+        return redirect(next_page)
+    return render_template('login.html', title='Sign In', form=form)
+@app.route('/')
+@app.route('/index')
+@login_required
+def index():
+    user = {'username': 'Garrett'}
+    logs = [
+        {
+        'worker': {'username': 'Doug'},
+        'description': '3 pallets of XPS Windows 512324 Laptops to Chicago'
+        },
+        {
+        'worker': {'username': 'Burt'},
+        'description': '125 Boxes of the Starburst Singles'
+         }
+        ]
+    return render_template('index.html', title='Home',user=user, logs=logs)
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
